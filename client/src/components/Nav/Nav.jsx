39fbd188b24d03d0c9cb0ce4eb55f43e4c5d93d9
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-10-05 19:47:39 -0400

    I added the navBar file in the components folder

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+ 
+function NavBarColor () {
+    return(
+        <>
+         <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#logout">logout</Nav.Link>
+            <Nav.Link href="#all videos">all videos</Nav.Link>
+            <Nav.Link href="#upload">upload a video</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+        </>
+    );
+   
+}
+export default NavBarColor;
