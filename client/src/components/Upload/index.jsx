fa188b8ebf5ba74fb359096318c491962bc78338
Ibrahima Diallo <ibrahimadiallo2394@gmail.com>
2023-10-11 20:32:27 -0400

    I fixed upload index.jsx file to upload a video and play it

--- client/src/components/Upload/index.jsx
+++ client/src/components/Upload/index.jsx
@@ -1,31 +1,57 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import {
   Form,
   Button,
   FormGroup,
   Label,
   Input,
-  FormText
+  FormText,
 } from 'reactstrap';
 
 function Upload() {
-  return (<Form><FormGroup>
-    <Label for="exampleFile">
-      File
-    </Label>
-    <Input
-      id="exampleFile"
-      name="file"
-      type="file"
-    />
-    <FormText>
-      This is some placeholder block-level help text for the above input. Its a bit lighter and easily wraps to a new line.
-    </FormText>
-  </FormGroup>
-    <Button color="primary" type="submit">
-      Submit
-    </Button>
-  </Form>);
-};
+  const [videoURL, setVideoURL] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const videoObjectURL = URL.createObjectURL(file);
+    setVideoURL(videoObjectURL);
+  };
+
+  return (
+    <div>
+      <Form>
+        <FormGroup>
+          <Label for="exampleFile">Upload Video</Label>
+          <Input
+            type="file"
+            name="file"
+            id="exampleFile"
+            accept="video/*"
+            onChange={handleFileChange}
+          />
+          <FormText>
+            Upload a video file (MP4, WebM, etc.).
+          </FormText>
+        </FormGroup>
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+      {videoURL && (
+        <div>
+          <h2>Uploaded Video:</h2>
+          <ReactPlayer
+            url={videoURL}
+            width="100%"
+            height="400px"
+            controls={true}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Upload;
