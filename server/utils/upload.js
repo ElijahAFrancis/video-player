c6a942dab1373742a8c0f8fce4d599fccc3cb17a
[Elijah <[efrancis64@gmail.com]>
2023-10-12 20:26:51 -0400

    merging

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,14 +1,14 @@
-const ffmpeg = require('fluent-ffmpeg');
+const multer = require('multer');
 
-// Function to convert video to MP4 format
-const convertToMP4 = (inputPath, outputPath) => {
-  return new Promise((resolve, reject) => {
-    ffmpeg(inputPath)
-      .output(outputPath)
-      .on('end', () => resolve(outputPath))
-      .on('error', (err) => reject(err))
-      .run();
-  });
-};
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.mp4');
+    }
+});
 
-module.exports = { convertToMP4 };
+const upload = multer({ storage: storage });
+
+module.exports = upload;
