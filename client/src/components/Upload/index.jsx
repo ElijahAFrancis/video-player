e78a002bc7bb41f81b45a4dc9923cf88f56cb101
MCKIT77 <jakemckit@gmail.com>
2023-10-18 17:48:17 -0400

    added public link appearing at bottom of video

--- client/src/components/Upload/index.jsx
+++ client/src/components/Upload/index.jsx
@@ -1,13 +1,94 @@
+// import React, { useState } from 'react';
+// import ReactPlayer from 'react-player';
+// import { useMutation } from '@apollo/client';
+// import { Form, Button, FormGroup, Label, Input, FormText } from 'reactstrap';
+// import { UPLOAD_VIDEO } from '../../utils/mutations'
+// const serverUrl = import.meta.env.BASE_URL
+// // const [uploadVideo] = useMutation(UPLOAD_VIDEO);
+
+// function Upload() {
+//   const [videoURL, setVideoURL] = useState(null);
+//   const [uploadVideo] = useMutation(UPLOAD_VIDEO);
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     const videoObjectURL = URL.createObjectURL(file);
+//     setVideoURL(videoObjectURL);
+//   };
+
+//   const handleUpload = async (e) => {
+//     e.preventDefault();
+
+//     const formData = new FormData();
+//     formData.append('file', e.target.file.files[0]);
+
+//     try {
+//       // Send the formData to the server
+//       // in development you must change `https://instaclip-5c26d78800f1.herokuapp.com/upload` to `http://${window.location.hostname}:3001/upload`
+//       const response = await fetch(`http://${window.location.hostname}:3001/upload`, {
+//         method: 'POST',
+//         body: formData,
+//       })
+
+//         .then((response) => response.json())
+//         .then((data) => {
+//           // Handle the server response, which may include the file path
+//           console.log('Server response:', data);
+
+//           // Call the GraphQL mutation with the received file path
+//           uploadVideo({ variables: { path: data.publicUrl } })
+//             .then((response) => {
+//               console.log('Video uploaded');
+//               // Handle the GraphQL response as needed
+//             })
+//             .catch((error) => {
+//               console.error('Error uploading video:', error);
+//               // Handle GraphQL errors
+//             });
+//         })
+//         .catch((error) => {
+//           console.error('Error uploading file:', error);
+//           // Handle file upload errors
+//         });
+//     } catch (error) {
+//       console.error('Error uploading file:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Form onSubmit={handleUpload}>
+//         <FormGroup>
+//           <Label for="exampleFile">Upload Video</Label>
+//           <Input
+//             type="file"
+//             name="file"
+//             id="exampleFile"
+//             accept="video/*"
+//             onChange={handleFileChange}
+//           />
+//           <FormText>Upload a video file (MP4, WebM, etc.).</FormText>
+//         </FormGroup>
+//         <Button color="primary" type="submit">
+//           Submit
+//         </Button>
+//       </Form>
+//       {videoURL && <ReactPlayer url={videoURL} controls />}
+//     </div>
+//   );
+// }
+
+// export default Upload;
+
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import { useMutation } from '@apollo/client';
 import { Form, Button, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { UPLOAD_VIDEO } from '../../utils/mutations'
-const serverUrl = import.meta.env.BASE_URL
-// const [uploadVideo] = useMutation(UPLOAD_VIDEO);
 
 function Upload() {
   const [videoURL, setVideoURL] = useState(null);
+  const [publicLink, setPublicLink] = useState(null); // Added state to store public link
   const [uploadVideo] = useMutation(UPLOAD_VIDEO);
 
   const handleFileChange = (e) => {
@@ -23,32 +104,34 @@ function Upload() {
     formData.append('file', e.target.file.files[0]);
 
     try {
-      // Send the formData to the server
+      // in development you must change `https://instaclip-5c26d78800f1.herokuapp.com/upload` to `http://${window.location.hostname}:3001/upload`
       const response = await fetch(`https://instaclip-5c26d78800f1.herokuapp.com/upload`, {
         method: 'POST',
         body: formData,
       })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the server response, which may include the file path
+          console.log('Server response:', data);
 
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the server response, which may include the file path
-        console.log('Server response:', data);
-  
-        // Call the GraphQL mutation with the received file path
-        uploadVideo({ variables: { path: data.publicUrl } })
-          .then((response) => {
-            console.log('Video uploaded');
-            // Handle the GraphQL response as needed
-          })
-          .catch((error) => {
-            console.error('Error uploading video:', error);
-            // Handle GraphQL errors
-          });
-      })
-      .catch((error) => {
-        console.error('Error uploading file:', error);
-        // Handle file upload errors
-      });
+          // Set the public link in state
+          setPublicLink(data.publicUrl);
+
+          // Call the GraphQL mutation with the received file path
+          uploadVideo({ variables: { path: data.publicUrl } })
+            .then((response) => {
+              console.log('Video uploaded');
+              // Handle the GraphQL response as needed
+            })
+            .catch((error) => {
+              console.error('Error uploading video:', error);
+              // Handle GraphQL errors
+            });
+        })
+        .catch((error) => {
+          console.error('Error uploading file:', error);
+          // Handle file upload errors
+        });
     } catch (error) {
       console.error('Error uploading file:', error);
     }
@@ -73,6 +156,7 @@ function Upload() {
         </Button>
       </Form>
       {videoURL && <ReactPlayer url={videoURL} controls />}
+      {publicLink && <p>Public Link: {publicLink}</p>} {/* Display the public link */}
     </div>
   );
 }
